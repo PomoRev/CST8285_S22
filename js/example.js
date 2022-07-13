7282a5fa7655b15f13368c89c2967cf21e5d9e1a
Dr. Frank Emanuel <emanuef@algonquincollege.com>
2022-07-13 15:13:32 -0400

    json stuff maybe

--- js/example.js
+++ js/example.js
@@ -1,7 +1,19 @@
 // Example of JavaScript bound to an HTML element
 
+
+const inspiration = {
+    "quotes": [
+        {"quote":  "potatoe", "author": "someone"},
+        {"quote":  "carrot", "author": "somelse"},
+        {"quote":  "cucumber", "author": "sometwo"},
+        {"quote":  "beet", "author": "somethree"},
+    ]
+};
+
 function helloWorld() {
 
-    alert("Hello World from JavaScript!");
+    int rndQuote = (Math.random() * inspiration.quotes.length)
+
+    alert(rndQuote);
 
 }
